f16faffe9e7cf61f52f15f44b6fc7a2c6ace44a0
Shreya-thakkar1 <shreyathakkar379@gmail.com>
2022-09-16 22:13:57 +0530

    Created seperate component and added css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  // const expenseDate = new Date(2022, 6, 3);
-  // const expenseTitle = "Medicine";
-  // const expensePrice = 278.1;
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      {/* <div>{props.date.toISOString()}</div> */}
+      <div>
+        <ExpenseDate date={props.date} />
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
