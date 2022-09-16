04e4c2ab606442956e2019b769e4e2f9db6026ca
Shreya-thakkar1 <shreyathakkar379@gmail.com>
2022-09-16 22:43:23 +0530

    assignment of adding new component from existing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
@@ -25,17 +26,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <p>Hello</p>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
+      <Expenses items={expenses} />
     </div>
   );
 }
