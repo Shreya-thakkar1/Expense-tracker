78151778bb9f1876a60409a364092665a9345303
Shreya-thakkar1 <shreyathakkar379@gmail.com>
2022-09-20 22:05:34 +0530

    Added form inputs

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,12 @@
+import ExpeseForm from "./ExpenseForm";
+import "./NewExpense.css";
+
+const NewFunction = () => {
+  return (
+    <div className="new-expense">
+      <ExpeseForm />
+    </div>
+  );
+};
+
+export default NewFunction;
