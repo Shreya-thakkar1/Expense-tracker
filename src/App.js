9698c668c3300bac9cb7647a69b8c4e1eeb2ad79
Shreya-thakkar1 <shreyathakkar379@gmail.com>
2022-09-21 22:49:18 +0530

    Key and added stateful lists

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
+import { useState } from "react";
 import Expenses from "./components/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+
+const DUMMY_EXPENSES = [
+  {
+    id: 1,
+    title: "Medicine",
+    amount: "200",
+    date: new Date(2022, 3, 27),
+  },
+  {
+    id: 2,
+    title: "PPF",
+    amount: "100",
+    date: new Date(2021, 3, 27),
+  },
+  {
+    id: 3,
+    title: "Mediclaim",
+    amount: "200",
+    date: new Date(2020, 3, 27),
+  },
+  {
+    id: 4,
+    title: "Mediclaim",
+    amount: "200",
+    date: new Date(2022, 3, 27),
+  },
+];
 function App() {
-  const expenses = [
-    {
-      id: 1,
-      title: "Medicine",
-      amount: "200",
-      date: new Date(2022, 3, 27),
-    },
-    {
-      id: 2,
-      title: "PPF",
-      amount: "100",
-      date: new Date(2021, 3, 27),
-    },
-    {
-      id: 3,
-      title: "Mediclaim",
-      amount: "200",
-      date: new Date(2020, 3, 27),
-    },
-    {
-      id: 3,
-      title: "Mediclaim",
-      amount: "200",
-      date: new Date(2022, 3, 27),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
   const addExpenseHandler = (expense) => {
     console.log("In App.js");
-    console.log(expense);
+    setExpenses((prevState) => {
+      return [expense, ...prevState];
+    });
   };
 
   return (
